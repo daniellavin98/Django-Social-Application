fa07e9c0f0309f05dd3140556c0704f3759501a2
daniellavin98 <danlavin98@gmail.com>
2024-05-01 09:28:56 -0600

    Feed changes

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create your model for social media site 
+
+class Post(models.Model):
+    text = models.CharField(max_length=250)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text[0:100]
+
